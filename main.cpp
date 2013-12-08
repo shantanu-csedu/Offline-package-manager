11117b46ff716c8d69ee597a7896a6880747a23c
shantanu saha <shantanucse18@gmail.com>
2013-12-08 14:25:59 +0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: shantanu
+ *
+ * Created on December 8, 2013, 2:17 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
